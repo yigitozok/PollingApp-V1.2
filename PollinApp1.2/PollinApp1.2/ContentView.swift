b11051710026f0c4ed92d2d4f1b60227f3a5eca7
yigitozok <yigitozok@hotmail.com>
2023-03-20 21:09:21 +0300

    Restart control

--- PollinApp1.2/PollinApp1.2/ContentView.swift
+++ PollinApp1.2/PollinApp1.2/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                     .ignoresSafeArea()
                 VStack(spacing: 125){
                     VStack(spacing: 25){
-                        Text("Poll")
+                        Text("Polling")
                             .font(.largeTitle)
                             .fontDesign(.monospaced)
                         Image(systemName: "checkmark.rectangle.portrait")
